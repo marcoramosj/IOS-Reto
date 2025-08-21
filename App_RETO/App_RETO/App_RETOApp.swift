3e5c146a9e130cae79c2b817d683907342bb5549
marcoramosj <marchomarco@hotmail.com>
2025-08-21 10:22:26 -0600

    Inicial

---
+++ App_RETO/App_RETO/App_RETOApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_RETOApp.swift
+//  App_RETO
+//
+//  Created by Marco Ramos Jalife on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct App_RETOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
