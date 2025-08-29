d8e9588d48c3c3dce8fbe024d2af8a04af0220e2
Jose Mauricio <Motrezt@gmail.com>
2025-08-28 20:46:09 -0600

    Interfaz de ventanilla

--- App_RETO/App_RETO/ContentView.swift
+++ App_RETO/App_RETO/ContentView.swift
@@ -9,15 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Datos aca bien locos muchos cambios")
+        NavigationStack {                    
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                Text("Datos aca bien locos muchos cambios")
+
+                NavigationLink("Ir a Inter") {
+                    InterfazVent()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
